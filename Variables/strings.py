7beee3993060e88292cc97963afb756dd9641ecc
Sergio Paglialunga <88593160+sergiopaglialunga@users.noreply.github.com>
2022-04-08 13:40:50 +0100

    String methods

---
+++ Variables/strings.py
@@ -0,0 +1,41 @@
+# # escape caracter
+# quotes = 'Eng said \n\t"He\'s good"'
+# # \n new line
+# # \t new tab
+# print(quotes)
+s = "Hello World"
+# c = s[6] # indexing
+# print(c)
+#
+# # string slicing
+# slice = s[6:9] # inclusive of 6 exclusive of 9
+# slice = s[:8]
+# slice = s[8:]
+# slice = s[1:10:2]
+slice = s[10::-1]
+#
+# # methods
+# s= "Engineering 110"
+#
+# print(s.lower())
+# print(s.upper())
+# print(s.capitalize())
+# print(s.strip())
+# print(s.replace('e', 'ooo').upper().replace('E','000'))
+
+print(slice)
+
+
+number = 10
+fruit = "apples"
+
+print(f"There are {str(number)} {fruit}")
+
+# name = input("What is your name?:\n")
+# age = input("How old are you?:\n")
+#
+# print(f"Your name is {name}, and your are {age} years old")
+
+# Use inputs to ask a few questions of the user
+# Try to collect text and numbers
+# Respond with a message that includes the input entries
---
+++ Variables/variables_intro.py
@@ -0,0 +1,22 @@
+# a = 1
+# b = 2
+# c = 3.5
+# d = "hello"
+#
+# # Dynamically Typed Language
+#
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+# Numeric Types
+
+a = 125 # int
+b = 1.25 # float
+c = 3 + 4j # complex
+
+print(c, type(c))
+
+# combine int and floats
+print(type(a+a))
